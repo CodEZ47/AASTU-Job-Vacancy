b7a7369272c4dd39e457accf87bf9518633bb9df
Yidnekachew Tebeje <yidnekachewtebeje@gmail.com>
2023-04-22 08:39:41 -0400

    created end point for user, positions

---
+++ Server/src/routes/positions.ts
@@ -0,0 +1,67 @@
+import prisma from "../lib/prisma";
+import express from "express";
+import { z } from "zod";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const positions = await prisma.position.findMany();
+  res.json(positions);
+});
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const position = await prisma.position.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  res.json(position);
+});
+router.post("/", async (req, res) => {
+  try {
+    const { name, description, level } = req.body;
+    const FormData = z.object({
+      name: z.string(),
+      description: z.string(),
+      level: z.string(),
+    });
+    const data = FormData.parse(req.body);
+    const position = await prisma.position.create({
+      data: {
+        name: name,
+        description: description,
+        level,
+      },
+    });
+    res.json(position);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+router.put("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { name, description, level } = req.body;
+        const FormData = z.object({
+            name: z.string(),
+            description: z.string(),
+            level: z.string(),
+        });
+        const position = await prisma.position.update({
+          where: {
+            id: id,
+          },
+          data: {
+            name: name,
+            description: description,
+            level,
+          },
+        });
+        res.json(position);
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
+ 
+});
+export default router;
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,49 +1,21 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-async function main() {
-  // Connect the client
-  await prisma.$connect()
-  await prisma.user.create({
-    data: {
-      name: 'Rich',
-      email: 'hello@prisma.com',
-      posts: {
-        create: {
-          title: 'My first post',
-          body: 'Lots of really interesting stuff',
-          slug: 'my-first-post',
-        },
-      },
-    },
-  })
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import * as position from './routes/positions';
+import * as user from './routes/user';
 const app = express();
-
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(helmet());
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
-
+app.use('/positions', position.default);
+app.use('/users', user.default);
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
