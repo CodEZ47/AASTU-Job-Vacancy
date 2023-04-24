7b0b942d555a838497a789233cb89edaeba4d8ef
Amanuel Kebede <amanuelabebe2000@gmail.com>
2023-04-24 05:09:39 +0300

    fixed some merge issues

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import {Homepage} from "./pages/admin/Homepage"
 import "./styles/style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import { Homepage } from "./pages/admin/Homepage";
-// import { UserDashboard } from "./pages/users/UserDashboard";
-import "./styles/style.css";
-
->>>>>>> main
 function App() {
   return (
     <>
